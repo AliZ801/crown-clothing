1679b63499721df0f6949b66a2d41446033cb1fc
Ali Zubair <61495779+AliZ801@users.noreply.github.com>
2020-07-30 11:31:03 +0300

    Added Redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './header.styles.scss';
 
-import {ReactComponent as Logo} from '../../assets/crown.svg';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 import auth from '../../firebase/firebase.utils';
 
 const Header = ({ currentUser }) => (
@@ -31,4 +32,8 @@ const Header = ({ currentUser }) => (
     </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
